aac3495768c9bf1a83c103c29377909073d08013
emanuelhardwell <emanuelnoe16@gmail.com>
2020-10-28 17:42:07 -0600

    Efect Scroll Reveal OK

---
+++ src/js/app.js
@@ -0,0 +1,42 @@
+/* 
+
+ */
+
+window.sr = ScrollReveal();
+
+sr.reveal(".home", {
+  duration: 2000,
+  origin: "bottom",
+});
+
+sr.reveal(".left", {
+  duration: 2000,
+  origin: "top",
+  distance: "300px",
+});
+
+sr.reveal(".right", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+
+sr.reveal(".comentario", {
+  duration: 2000,
+  delay: 500,
+  origin: "bottom",
+});
+
+sr.reveal(".testimonio", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+
+sr.reveal(".footer", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.2,
+});
